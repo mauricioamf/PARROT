4d048619246acce892dc252bc7446dfdb28dfac1
Mauricio Ferreira <mauricioferreira421@gmail.com>
2022-08-19 16:24:27 +0200

    Add files via upload

---
+++ Code/GECKO_Ecoli/geckomat/utilities/integrate_proteomics/getOxPhosRxnIDs.m
@@ -0,0 +1,13 @@
+function oxPhos = getOxPhosRxnIDs(model,parameters)
+if isfield(parameters,'oxPhos')
+    oxPhos = [];
+    for i=1:length(parameters.oxPhos)
+        ID = parameters.oxPhos{i};
+        isoEnzymes = model.rxns(find(contains(model.rxns,ID)));
+        isoEnzymes = isoEnzymes(~contains(isoEnzymes,'arm_'));
+        oxPhos = [oxPhos; isoEnzymes];
+    end
+else
+    oxPhos = [];
+end
+end
